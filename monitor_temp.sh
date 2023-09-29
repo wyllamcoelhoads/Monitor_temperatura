ed893a3f30850572bd745304aa9f8d6e1f902621
Williamads <39974819+wyllamcoelhoads@users.noreply.github.com>
2023-09-29 09:17:01 -0300

    Add files via upload
    relese
    
    agora ele informa ao usuario que o programa esta em execução e solicita que o terminal não seje encerrado 
    a criação de log esta sendo gerada automaticamente conforme data e hora

--- monitor_temp.sh
+++ monitor_temp.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Defina o caminho para o arquivo de log
-LOG_FILE="/home/william-coelho/Documents/MTEMP/temperatura.log"
+# Diretório onde os arquivos de log serão armazenados
+LOG_DIR="/home/william-coelho/Documents/MTEMP"
+# Prefixo do nome do arquivo de log
+LOG_PREFIX="temperatura"
+# Extensão do arquivo de log
+LOG_EXTENSION="log"
 # Defina o intervalo em segundos (exemplo: 10 segundos)
 INTERVAL=10
+
+# Crie um nome de arquivo com timestamp
+TIMESTAMP=$(date "+%Y%m%d%H%M%S")
+LOG_FILE="$LOG_DIR/$LOG_PREFIX-$TIMESTAMP.$LOG_EXTENSION"
 
 # Loop infinito para monitorar a temperatura
 while true; do
@@ -13,4 +21,6 @@ while true; do
     echo $(date "+%Y-%m-%d %H:%M:%S") >> "$LOG_FILE"
     # Espere pelo intervalo especificado
     sleep $INTERVAL
+clear
+echo "O programa esta em execução, não feche esse terminal! "
 done
